8278c4dc0ab06397ff2b037df43b741d98d3db4d
akhaten <khafifjessy.github@gmail.com>
2023-06-12 11:01:34 +0200

    Google Colab integration updated

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {}, ... }:
+
+pkgs.mkShell {
+
+    buildInputs = with pkgs; [
+
+        python310
+        python310Packages.numpy
+        python310Packages.scipy
+        python310Packages.scikitimage
+        python310Packages.matplotlib
+        python310Packages.pandas
+
+
+        # Package for Jupyter / To comment
+        python310Packages.ipywidgets
+        python310Packages.ipykernel
+        python310Packages.ipympl
+
+   
+
+        
+
+    ];
+
+}
